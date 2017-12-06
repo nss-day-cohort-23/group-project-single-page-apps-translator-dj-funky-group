e2b30098103eff52937dab3cd8eacee8661e944d
Melissa Bell <melissabell456@gmail.com>
2017-12-06 14:55:57 -0600

    commit prior to merge with master. added greek/DOM js

---
+++ javascripts/DOMinput.js
@@ -0,0 +1,11 @@
+// dom-input.js
+// - adds button click event listener to DOM translate button
+let translateBtn = document. getElementById();
+// - imports str from DOM textarea
+
+// - imports selection from DOM language options
+
+// - stores language selection as str
+
+// - exports textarea str, exports selection str
+// module.exports = {  };
