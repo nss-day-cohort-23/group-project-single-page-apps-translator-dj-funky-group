52a6f6b897bd37dcf16d4b9bb44d62bcd491876b
Melissa Bell <melissabell456@gmail.com>
2017-12-06 15:15:27 -0600

    commit prior to merge

--- javascripts/DOMinput.js
+++ javascripts/DOMinput.js
@@ -1,9 +1,20 @@
+"use strict";
 // dom-input.js
 // - adds button click event listener to DOM translate button
-let translateBtn = document. getElementById();
-// - imports str from DOM textarea
+let translateBtn = document.getElementById("translateBtn");
+translateBtn.addEventListener("click", )
 
-// - imports selection from DOM language options
+
+
+function textInput () {
+    let userInput = document.getElementById("textInput").innerText;
+    return userInput
+}
+
+function getLanguage () {
+    let selectedLanguage = document.getElementById("langOption").value;
+    return selectedLanguage
+}
 
 // - stores language selection as str
 
