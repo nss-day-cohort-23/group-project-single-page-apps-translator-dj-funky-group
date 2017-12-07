20a4cee72e332691a767f532518aa1dbdf65b79e
Melissa Bell <melissabell456@gmail.com>
2017-12-07 09:57:09 -0600

    greek pg built. translator rough draft added

--- javascripts/DOMinput.js
+++ javascripts/DOMinput.js
@@ -7,16 +7,18 @@ let translateBtn = document.getElementById("translateBtn");
 translateBtn.addEventListener("click", function () {
     text = textInput();
     lang = getLanguage();
+    console.log(text);
+    console.log(lang);
 });
 
 function textInput () {
     let userInput = document.getElementById("textInput").value;
-    return userInput
+    return userInput;
 }
 
 function getLanguage () {
     let selectedLanguage = document.getElementById("langOption").value;
-    return selectedLanguage
+    return selectedLanguage;
 }
 
 
--- javascripts/greek.js
+++ javascripts/greek.js
@@ -1,2 +1,10 @@
 "use strict";
-// Happy New Year
+console.log("linked");
+
+let greek = {Happy: "Eftychismeno", New: "to neo", Year: "etos" };
+// turn string of user entry into array, each word will then be passed into this function to create translated string
+function getGreekWord (word) {
+    return greek[word];
+}
+
+module.exports=getGreekWord;
