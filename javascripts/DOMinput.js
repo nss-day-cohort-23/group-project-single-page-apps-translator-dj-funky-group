212a592db4c9ed1ffa665596d57d734a987b6da1
David Lars Ketch <davidlarsketch@gmail.com>
2017-12-07 13:20:28 -0600

    Removes addEventListener & restructures exports

--- javascripts/DOMinput.js
+++ javascripts/DOMinput.js
@@ -1,25 +1,13 @@
 "use strict";
 
-let text;
-let lang;
-
-let translateBtn = document.getElementById("translateBtn");
-translateBtn.addEventListener("click", function () {
-    text = textInput();
-    lang = getLanguage();
-    console.log(text);
-    console.log(lang);
-});
-
 function textInput () {
-    let userInput = document.getElementById("textInput").value.toLowerCase();
-    return userInput;
+  let userInput = document.getElementById("textInput").value.toLowerCase();
+  return userInput;
 }
 
 function getLanguage () {
-    let selectedLanguage = document.getElementById("langOption").value;
-    return selectedLanguage;
+  let selectedLanguage = document.getElementById("langOption").value;
+  return selectedLanguage;
 }
 
-
-module.exports = { text, lang };
+module.exports = { textInput, getLanguage };
